ec44d090ff4454205a6a3a6f3b8e28814d7f6ebf
TotsukenLeo <totsuken@mail.ru>
2021-12-27 11:19:28 +0500

    first commit

---
+++ lib/screens/quiz/quiz_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:websafe_svg/websafe_svg.dart';
+
+import 'components/body.dart';
+
+class QuizScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: [
+          FlatButton(onPressed: () {}, child: Text("Skip")),
+        ],
+      ),
+      body: Body(),
+    );
+  }
+}
+
---
+++ lib/screens/welcome/welcome_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:quiz_app/constants.dart';
+import 'package:websafe_svg/websafe_svg.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          WebsafeSvg.asset("assets/icons/bg.svg", width: 450),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Spacer(),
+                  Text(
+                    "Let's Plat Quiz,",
+                    style: Theme.of(context).textTheme.headline4.copyWith(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  Text("Enter your informations below"),
+                  Spacer(flex: 2),
+                  TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xFF1C2341),
+                      hintText: "Full Name",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
+                      ),
+                    ),
+                  ),
+                  Spacer(),
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      width: double.infinity,
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.all(kDefaultPadding * 0.75),
+                      decoration: BoxDecoration(
+                        gradient: kPrimaryGradient,
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
+                      ),
+                      child: Text(
+                        "Let's Start Quiz",
+                        style: Theme.of(context)
+                            .textTheme
+                            .button
+                            .copyWith(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                  Spacer(flex: 2),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
